a22e73ffdadb15268e0c34caacd7ded6f682ce1d
Rick Petzold <ricken38@gmail.com>
2025-07-25 08:38:51 +0200

    preflight cors

--- backend/app/server.js
+++ backend/app/server.js
@@ -20,6 +20,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.sendStatus(200);
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
